10a4b3fcc8c6c183c55dbd94e2e611fddcc16fd8
safwan-ms <safwan.anasms@gmail.com>
2024-10-26 11:40:40 +0530

    added the mini project of the changing background theme

---
+++ 04. Toggle Background Color/src/components/ToggleBackgroundColor.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../style.css";
+const ToggleBackgroundColor = () => {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [textColor, setTextColor] = useState("#1b1b1b");
+  const [buttonStyle, setButtonStyle] = useState("white");
+
+  const handleClick = () => {
+    setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
+    setTextColor(textColor === "#1b1b1b" ? "#ffa31a" : "#1b1b1b");
+    setButtonStyle(backgroundColor === "white" ? "#1b1b1b" : "white");
+  };
+
+  return (
+    <div style={{ backgroundColor, color: textColor }}>
+      <button
+        onClick={handleClick}
+        style={{
+          buttonStyle,
+          color: textColor,
+          border: `3px solid ${textColor}`,
+        }}
+      >
+        {backgroundColor == "white" ? "Black Theme" : "White Theme"}
+      </button>
+      <section className="content">
+        <h1>
+          Welcome To A <br />
+          Real World
+        </h1>
+      </section>
+    </div>
+  );
+};
+
+export default ToggleBackgroundColor;
