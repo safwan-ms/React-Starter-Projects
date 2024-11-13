6ade8f7502ca1a849f8eca6cba3ddb25e78be93e
safwan-ms <safwan.anasms@gmail.com>
2024-11-13 10:32:17 +0530

    refactor: added clear button functionality

--- 09. Star Rating/src/components/StarRating.jsx
+++ 09. Star Rating/src/components/StarRating.jsx
@@ -3,22 +3,32 @@ import { FaStar } from "react-icons/fa";
 
 const StarRating = ({ noOfStars }) => {
   const [rating, setRating] = useState(0);
-  const handleClick = (findCurrentIndex) => {
-    setRating(findCurrentIndex);
+  const handleClick = (getCurrentIndex) => {
+    setRating(getCurrentIndex);
   };
   return (
-    <div className="flex justify-center">
-      {[...Array(noOfStars)].map((_, index) => {
-        index += 1;
-        return (
-          <FaStar
-            size={40}
-            onClick={() => handleClick(index)}
-            onMous
-            className={index <= rating ? "text-yellow-500" : "text-black"}
-          />
-        );
-      })}
+    <div>
+      <div className="flex items-center justify-center w-full h-screen">
+        {[...Array(noOfStars)].map((_, index) => {
+          index += 1;
+          return (
+            <FaStar
+              className={
+                index <= rating ? "text-yellow-500 mr-5" : "text-black mr-5"
+              }
+              onClick={() => handleClick(index)}
+              key={index}
+              size={40}
+            />
+          );
+        })}
+        <button
+          onClick={() => setRating(0)}
+          className="px-5 py-3 text-lg font-bold text-white bg-red-700 rounded-md"
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
 };
